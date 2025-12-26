3e8d464465a4cf811df70d78bf54f02adccc049d
oliullahakib <oliullahakib@gmail.com>
2025-12-26 17:04:40 +0600

    added metadata to reviews and home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,3 +15,7 @@ export default function Home() {
     </div>
   );
 }
+export const metadata = {
+  title: "Home | Yantun Kaijan",
+  description: "Home page of Yantun Kaijan ",
+};
--- src/app/reviews/page.jsx
+++ src/app/reviews/page.jsx
@@ -1,36 +1,13 @@
-"use client";
-import ReviewCard from "@/components/cards/ReviewCard";
-import React, { useEffect, useState } from "react";
-import ReviewLoading from "./ReviewLoading";
+import Reviews from "./Reviews";
 
 const ReviewsPage = () => {
-  const [reviews, setReviews] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetch("https://taxi-kitchen-api.vercel.app/api/v1/reviews")
-      .then((res) => res.json())
-      .then((data) => {
-        setReviews(data.reviews || []);
-        setLoading(false);
-      });
-  }, []);
 
-  if (loading) {
-    return <ReviewLoading></ReviewLoading>;
-  }
-  return (
-    <div>
-      <h2 className="text-4xl font-bold">
-        Total <span className="text-yellow-500">{reviews.length} </span> Reviews
-        Found
-      </h2>
-      <div className="grid my-5 grid-cols-3 gap-5">
-        {reviews.map((rev) => (
-          <ReviewCard review={rev} key={rev.id}></ReviewCard>
-        ))}
-      </div>
-    </div>
-  );
+  return <Reviews></Reviews>
 };
 
 export default ReviewsPage;
+
+export const metadata = {
+  title: "Reviews | Yantun Kaijan",
+  description: "Reviews page of Yantun Kaijan ",
+};
